195e1c682c55ffda3598b1918c58573fca2b1ba1
daveym <davey.mcglade@gmail.com>
2016-04-09 11:54:30 +0100

    Common chunks plugin

--- webpk/src/index.js
+++ webpk/src/index.js
@@ -1,13 +1,22 @@
-import $ from 'jquery';
-import Button from './Components/Button';
+	import $ from 'jquery';
+	import Button from './Components/Button';
 
-// Code Splitting
+	// Code Splitting
+// If we have an anchor, render the Button component on it
 if (document.querySelectorAll('a').length) {
     require.ensure([], () => {
-        const Button = require('./Components/Button').default;
+        const Button = require('./Components/Button');
         const button = new Button('google.com');
 
         button.render('a');
     });
 }
 
+// If we have a title, render the Header component on it
+if (document.querySelectorAll('h1').length) {
+    require.ensure([], () => {
+        const Header = require('./Components/Header');
+
+        new Header().render('h1');
+    });
+}
